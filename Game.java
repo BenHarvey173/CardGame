7e7d44cdd87ac44b5ff94bc16f9ea490ea3f28a3
BenHarvey173 <142693051+BenHarvey173@users.noreply.github.com>
2024-04-29 15:05:12 +0000

    Hlp

--- Game.java
+++ Game.java
@@ -1,10 +1,13 @@
 
+import java.util.Scanner;
 public class Game {
     public static void playHoldEm(int players) {
         HoldEm holdEm = new HoldEm(players);
-        for(int i = 0; i < players; i++) {
-            System.out.print(holdEm.getHand(i));
+        int[] money = new int[players];
+        for (int i = 0; i < money.length; i++) {
+            money[i] = 5000;
         }
+        playRound(players, money);
         System.out.println("");
         System.out.println(holdEm.drawRiver());
         System.out.println(holdEm.drawRiver());
@@ -14,6 +17,69 @@ public class Game {
         holdEm.scoreHands();
         
    
+    }
+    public static void playRound(int players, int[] money, int pot) {
+        boolean[] in = new boolean[players];
+        int[] paid = new int[players];
+        for (int i = 0; i < players; i++) {
+            in[i] = true;
+        }
+        Scanner sc = new Scanner(System.in);
+        int bet = 0;
+        while (true) {
+            for (int i = 0; i < players ; i++) {
+                if(!in[i]) {
+                    continue;
+                }
+                System.out.println("Player " + i + "'s Turn:");
+                System.out.println("Bet is $" + bet + ".\n\t1. Call\n\t2. Raise\n\t3. Fold");
+                int choice = sc.nextInt();
+                System.out.println(choice);
+                if(choice == 1) {
+                    if (money[i] < bet) {
+                        System.out.println("Insufficient funds. Go all in?\n\t1. Yes\n\t2. No");
+                        int allIn = sc.nextInt();
+                        if(allIn == 1) {
+                            pot += money[i];
+                            paid[i] += money[i];
+                            money[i] = 0;
+                        } else {
+                            in[i] = false;
+                        }
+                        continue;
+                    }
+                    money[i] -= (bet - paid[i]);
+                    pot += (bet - paid[i]);
+                    paid[i] = bet;
+                    continue;
+                } else if (choice == 2) {
+                    System.out.println("Input bet amount:\n\t$");
+                    bet = sc.nextInt();
+                    if (money[i] < bet) {
+                        System.out.println("Insufficient funds. Go all in?\n\t1. Yes\n\t2. No");
+                        int allIn = sc.nextInt();
+                        if(allIn == 1) {
+                            pot += money[i];
+                            paid[i] += money[i];
+                            money[i] = 0;
+                        } else {
+                            in[i] = false;
+                        }
+                        continue;
+                    }
+                    money[i] -= (bet - paid[i]);
+                    pot += (bet - paid[i]);
+                    paid[i] = bet;
+                    continue;
+                } else {
+                    in[i] = false;
+                    continue;
+                }
+
+            }
+            
+        }
+        sc.close();
     }
     public static void playBlackJack() {
         
